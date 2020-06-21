4c00ec154deb7af1ead11143a6580391cde5d6d2
Cleysson Azevedo <cleyssonazevedo@outlook.com>
2020-06-21 15:59:32 -0300

    Correção lint

--- src/app/pages/financas/financas-resolver.service.ts
+++ src/app/pages/financas/financas-resolver.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Injectable()
