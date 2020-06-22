79ed531941511af1b8410166b2f3feb5082073bc
Cleysson Azevedo <cleyssonazevedo@outlook.com>
2020-06-21 22:09:53 -0300

    Otimização

--- src/app/components/nova-financa/nova-financa.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-nova-financa',
-    templateUrl: './nova-financa.component.html',
-    styleUrls: [ './nova-financa.component.scss' ]
-})
-export class NovaFinancaComponent {  }
